d8b8216f63c1879ca9795a58e3eb33d7b951b0ae
miyandac <lornamiyanda@gmail.com>
2019-10-26 21:52:32 +0530

    New application
    Farmer system

---
+++ src/main/java/zm/co/farmer/farmnet/FarmnetApplication.java
@@ -0,0 +1,13 @@
+package zm.co.farmer.farmnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmnetApplication.class, args);
+	}
+
+}
