f2d52de51291cbc56aba196927235673ae1faad6
Mumbi Chishimba <mumbi.chishimba@nipa.ac.zm>
2019-10-27 17:48:15 +0530

    CHANGES Farmer entity

--- src/main/java/zm/co/farmer/farmnet/entity/Farmer.java
+++ src/main/java/zm/co/farmer/farmnet/entity/Farmer.java
@@ -5,6 +5,12 @@
  */
 package zm.co.farmer.farmnet.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author MUNDIAG
