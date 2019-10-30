54308b8fd0341cc11dae01a27ac3371283b1a496
Mumbi Chishimba <mumbi.chishimba@nipa.ac.zm>
2019-10-30 22:10:15 +0530

    Entities
    Don't do drugs boys and girls

---
+++ src/main/java/zm/co/farmer/farmnet/service/InventoryService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.co.farmer.farmnet.service;
+
+/**
+ *
+ * @author Mumbi Chishimba
+ */
+public class InventoryService {
+    
+}
