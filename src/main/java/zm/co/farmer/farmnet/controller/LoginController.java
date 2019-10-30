77b21fe02bed70bbca2c56a84d4e40384257a9a6
Dhulfa Abdallah <saidsanura94@gmail.com>
2019-10-30 20:04:15 +0300

    Login
    Login

--- src/main/java/zm/co/farmer/farmnet/controller/LoginController.java
+++ src/main/java/zm/co/farmer/farmnet/controller/LoginController.java
@@ -8,7 +8,9 @@ package zm.co.farmer.farmnet.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import zm.co.farmer.farmnet.service.UserService;
 
 /**
@@ -17,18 +19,24 @@ import zm.co.farmer.farmnet.service.UserService;
  */
 @Controller
 public class LoginController {
-    
+
     @Autowired
     private UserService userService;
 
-    
     @RequestMapping(path = {"/"})
     public String homepage(Model model) {
         return "login";
     }
-    
-    @RequestMapping(path = {"/login"})
+
+    @RequestMapping(path = {"/login"}, method = RequestMethod.GET)
     public String login(Model model) {
+        return "login";
+    }
+
+    @RequestMapping(path = {"/login"}, method = RequestMethod.POST)
+    public String loginAction(Model model,@ModelAttribute("username")String username,@ModelAttribute("password")String password) {
+        System.out.println("Username " +username);
+        System.out.println("Password " +password);
         return "login";
     }
 
@@ -39,10 +47,10 @@ public class LoginController {
 
     @RequestMapping(path = {"/pageone"})
     public String pageone(Model model) {
-        
+
         // Inject user list
         model.addAttribute("users", userService.getAllUsers());
-        
+
         return "pageone";
     }
 }
