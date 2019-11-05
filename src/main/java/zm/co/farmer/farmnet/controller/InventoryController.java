49760574fff7011b6721daa7ed297f3296229280
Mumbi Chishimba <mumbi.chishimba@nipa.ac.zm>
2019-11-05 14:06:13 +0530

    Login
    Login function is now working

---
+++ src/main/java/zm/co/farmer/farmnet/controller/InventoryController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zm.co.farmer.farmnet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import zm.co.farmer.farmnet.entity.User;
+import zm.co.farmer.farmnet.service.UserService;
+
+/**
+ *
+ * @author Mumbi Chishimba
+ */
+
+@Controller
+public class InventoryController {
+    
+    @Autowired
+    private UserService userService;
+    
+    @RequestMapping(path = {"/addinventory"}, method = RequestMethod.GET)
+    public String loginAction(Model model, @CookieValue("user_session_id") String username) {
+        
+        User user = userService.getUserByUsername(username);
+        model.addAttribute("user", user);
+        
+        return "addinventoryitem";
+    }
+    
+}
--- src/main/java/zm/co/farmer/farmnet/controller/LoginController.java
+++ src/main/java/zm/co/farmer/farmnet/controller/LoginController.java
@@ -5,12 +5,18 @@
  */
 package zm.co.farmer.farmnet.controller;
 
+import java.util.logging.Level;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import zm.co.farmer.farmnet.entity.User;
 import zm.co.farmer.farmnet.service.UserService;
 
 /**
@@ -34,14 +40,53 @@ public class LoginController {
     }
 
     @RequestMapping(path = {"/login"}, method = RequestMethod.POST)
-    public String loginAction(Model model,@ModelAttribute("username")String username,@ModelAttribute("password")String password) {
-        System.out.println("Username " +username);
-        System.out.println("Password " +password);
-        return "login";
+    public String loginAction(HttpServletRequest request,
+            HttpServletResponse response,
+            Model model,
+            @ModelAttribute("username") String username,
+            @ModelAttribute("password") String password) throws Exception {
+
+        try {
+            User user = userService.login(username, password);
+            if (user == null) {
+                return "redirect:/login?error=invalidusernamepassword";
+            }
+
+            model.addAttribute("user", user);
+
+            if (user == null) {
+                return "login?error=invalidusernamepassword";
+            } else {
+
+                //user.setPassword("");
+                request.getSession().setAttribute("logged_in_user", user);
+
+                String id = user.getId().toString();
+
+                Cookie cookie = new Cookie("user_session_id", username);
+
+                cookie.setHttpOnly(true);
+                cookie.setMaxAge(36000);
+
+                response.addCookie(cookie);
+            }
+
+            // Redirect user
+            return "redirect:/home";
+
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+            return "redirect:/login?error=invalidusernamepassword";
+        }
     }
 
     @RequestMapping(path = {"/home"})
-    public String home(Model model) {
+    public String home(Model model, @CookieValue("user_session_id") String username) {
+        
+        User user = userService.getUserByUsername(username);
+        model.addAttribute("user", user);
+        
+        
         return "home";
     }
 
